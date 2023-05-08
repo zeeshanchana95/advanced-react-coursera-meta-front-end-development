c8256005f6ddda7e61f7b5b1bd33c15f3a4f1fab
Zeeshan Ahmed Channa <zeeshanchana95@gmail.com>
2023-05-08 18:05:55 +0500

    Revising useState Hook

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import { useState } from "react";
 import "./App.css";
 
 
-function App() {
+export default function App() {
+
+  const [restaurantName, setRestaurantName] = useState("Little");
+  
+  function updateRestaurantName(){
+    setRestaurantName("Little Lemon")
+  };
+
   return (
     <div className="App">
-      
+      <h1>{restaurantName}</h1>
+      <button onClick={updateRestaurantName}>
+        Update Restaurant Name
+      </button>
     </div>
   );
 }
-
-export default App;
